995403b3a0a00d7b6791fd3c679ba793df99fa0a
Tiberiu Ichim <tiberiu.ichim@eaudeweb.ro>
2012-11-23 10:23:59 +0100

    Fixed version handling by reading __file__

--- sparql.py
+++ sparql.py
@@ -58,7 +58,7 @@ import re
 import urllib2
 
 
-__version__ = open(os.path.join('version.txt')).read().strip()
+__version__ = open(os.path.join(os.path.dirname(__file__), 'version.txt')).read().strip()
 
 USER_AGENT =  "sparql-client/%s +http://www.eionet.europa.eu/software/sparql-client/" % __version__
 
